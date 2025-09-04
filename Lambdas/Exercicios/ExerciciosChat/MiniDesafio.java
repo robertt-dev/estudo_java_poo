a4c7733c13e5a9fa5c951626ba75e278cb938c3c
robertt-dev <roberttmehary.dev@gmail.com>
2025-09-04 13:51:57 -0300

    revisão lambdas 02

---
+++ Lambdas/Exercicios/ExerciciosChat/MiniDesafio.java
@@ -0,0 +1,15 @@
+package Lambdas.Exercicios.ExerciciosChat;
+
+public class MiniDesafio {
+    
+}
+
+
+
+
+// Mini Desafio
+
+// Combine Predicate e Consumer:
+
+// Faça um Predicate<String> que verifica se um nome tem mais de 5 letras.
+// Se o nome passar, use um Consumer<String> para imprimir "Nome aprovado: X".
--- Lambdas/Exercicios/ExerciciosChat/Predicate05.java
+++ Lambdas/Exercicios/ExerciciosChat/Predicate05.java
@@ -1,7 +1,22 @@
 package Lambdas.Exercicios.ExerciciosChat;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Predicate05 {
-    
+
+    public static void main(String[] args){
+
+        List<Integer> numerosRandom = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        Predicate<Integer> ehPar = numero -> { return numero % 2 == 0; };
+
+        for(int i = 0; i < numerosRandom.size(); i++) {
+            if(ehPar.test(numerosRandom.get(i))) {
+                System.out.println(numerosRandom.get(i));
+            }
+        }
+    }
 }
 
 
